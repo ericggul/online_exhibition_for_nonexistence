43e1ce3eeb2ec6f2be487ec5836f8ca1924a0b50
1214sw <43122459+1214sw@users.noreply.github.com>
2021-01-29 18:12:52 +0900

    첫주에 공부한 것들
    ㅇㅅㅇ

---
+++ web2-node.js/main.js
@@ -0,0 +1,21 @@
+/*
+cd C:\Users\1214sw\Documents\2020 winter\SoMa\web2-node.js
+node main.js
+*/
+
+var http = require('http');
+var fs = require('fs');
+var app = http.createServer(function(request,response){
+    var url = request.url;
+    if(request.url == '/'){
+      url = '/index.html';
+    }
+    if(request.url == '/favicon.ico'){
+      return response.writeHead(404);
+    }
+    response.writeHead(200);
+    console.log(__dirname + url);
+    response.end(fs.readFileSync(__dirname + url));
+
+});
+app.listen(3000);
